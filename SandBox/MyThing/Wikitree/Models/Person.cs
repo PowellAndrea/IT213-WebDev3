c7a00df107813e45ad6e2cf35240613208e48cf3
PowellAndrea <80601370+PowellAndrea@users.noreply.github.com>
2022-02-06 15:03:23 -0800

    part 8

---
+++ SandBox/MyThing/Wikitree/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Wikitree.Models
+{
+   public class Person
+   {
+      public Guid Id { get; set; }
+      public string CommonName { get; set; }
+      public Relationship[] Relationships { get; set; }
+      
+   }
+}
---
+++ SandBox/MyThing/Wikitree/Models/Relationship.cs
@@ -0,0 +1,11 @@
+﻿namespace Wikitree.Models
+{
+   public class Relationship
+   {
+      public Guid Id { get; set; }
+      public string Person1;
+      public string Person1Role;
+      public string Person2;
+      public string Person2Role;
+   }
+}
